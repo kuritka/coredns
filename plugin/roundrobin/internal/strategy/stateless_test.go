c8de59f2867602be54681458cda36ab7b1004dab
kuritka <kuritka@gmail.com>
2022-04-26 16:00:29 +0200

    stateless tests

--- plugin/roundrobin/internal/strategy/stateless_test.go
+++ plugin/roundrobin/internal/strategy/stateless_test.go
@@ -33,89 +33,99 @@ func TestRoundRobinStatelessInitialize(t *testing.T) {
 		{`blah=`, expected},
 	}
 	for _, raw := range testValues {
-		m := newMid()
+		t.Run(fmt.Sprintf("with%s", raw.value), func(t *testing.T) {
+			m := newMid()
 
-		m.AddResponseAnswer(cname)
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.1"))
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.2"))
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.3"))
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.4"))
-		m.AddResponseAnswer(mx)
-		m.AddRequestOptRaw(raw.value)
+			m.AddResponseAnswer(cname)
+			m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.1"))
+			m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.2"))
+			m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.3"))
+			m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.4"))
+			m.AddResponseAnswer(mx)
+			m.AddRequestOptRaw(raw.value)
 
-		var clientState = NewStateless().Shuffle(m.req, m.res)
+			var clientState = NewStateless().Shuffle(m.req, m.res)
 
-		if len(clientState) != len(m.res.Answer) {
-			t.Errorf("The stateless retrieved different number of records. Expected %v got %v", len(m.res.Answer), len(clientState))
-		}
+			if len(clientState) != len(m.res.Answer) {
+				t.Errorf("The stateless retrieved different number of records. Expected %v got %v", len(m.res.Answer), len(clientState))
+			}
 
-		if fmt.Sprintf("%v", getIPs(clientState)) != raw.expected {
-			t.Errorf("The stateless shuffle is not working as expected. For %s Expecting %v but got %v.", raw.value, expected, getIPs(clientState))
-		}
+			if fmt.Sprintf("%v", getIPs(clientState)) != raw.expected {
+				t.Errorf("The stateless shuffle is not working as expected. For %s Expecting %v but got %v.", raw.value, expected, getIPs(clientState))
+			}
 
-		// end of the list contains additional records in the order of they are defined in the response
-		if clientState[4].String() != cname.String() {
-			t.Errorf("Expecting %s result but got %s", cname, clientState[4].String())
-		}
-		if clientState[5].String() != mx.String() {
-			t.Errorf("Expecting %s result but got %s", mx, clientState[5].String())
-		}
+			// end of the list contains additional records in the order of they are defined in the response
+			if clientState[4].String() != cname.String() {
+				t.Errorf("Expecting %s result but got %s", cname, clientState[4].String())
+			}
+			if clientState[5].String() != mx.String() {
+				t.Errorf("Expecting %s result but got %s", mx, clientState[5].String())
+			}
+		})
 	}
 }
 
-func TestRoundRobinStatelessShuffleA(t *testing.T) {
-	var arotations = []string{"[10.240.0.2 10.240.0.3 10.240.0.4 10.240.0.1]", "[10.240.0.3 10.240.0.4 10.240.0.1 10.240.0.2]",
-		"[10.240.0.4 10.240.0.1 10.240.0.2 10.240.0.3]", "[10.240.0.1 10.240.0.2 10.240.0.3 10.240.0.4]"}
-	var rr []dns.RR
-	for i := 0; i < 10; i++ {
-		m := newMid()
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.1"))
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.2"))
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.3"))
-		m.AddResponseAnswer(test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.4"))
-		// state, from the previous loop that arrived in the DNS query
-		m.AddRequestOpt(rr...)
-		var clientState = NewStateless().Shuffle(m.req, m.res)
-
-		// save the new state for the next query
-		rr = filterAandAAAA(clientState)
-
-		if fmt.Sprintf("%v", getIPs(clientState)) != fmt.Sprintf("%v", arotations[i%len(arotations)]) {
-			t.Errorf("%v: The stateless rotation is not working. Expecting %v but got %v.", i, arotations[i%len(arotations)], getIPs(clientState))
-		}
+func TestRoundRobinStatelessShuffle(t *testing.T) {
+	tests := []struct {
+		name             string
+		answer           []dns.RR
+		expectedResponse []string
+		expectedNonAPositionsMapping map[int]int
+		requestOpt       []dns.RR
+	}{
+		{"A records",
+			[]dns.RR{
+				test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.1"),
+				test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.2"),
+				test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.3"),
+				test.A("alpha.cloud.example.com.		300	IN	A			10.240.0.4"),
+			}, []string{"[10.240.0.2 10.240.0.3 10.240.0.4 10.240.0.1]", "[10.240.0.3 10.240.0.4 10.240.0.1 10.240.0.2]",
+				"[10.240.0.4 10.240.0.1 10.240.0.2 10.240.0.3]", "[10.240.0.1 10.240.0.2 10.240.0.3 10.240.0.4]"},
+			map[int]int{},
+			[]dns.RR{},
+		},
+		{
+			"AAAA and Non AAAA records",
+			[]dns.RR{
+				test.AAAA("ipv6.cloud.example.com.		300	IN	AAAA			4001:a1:1014::89"),
+				test.CNAME("ipv6.cloud.example.com.	300	IN	CNAME		beta.cloud.example.com."),
+				test.AAAA("ipv6.cloud.example.com.		300	IN	AAAA			4001:a1:1014::8a"),
+				test.AAAA("ipv6.cloud.example.com.		300	IN	AAAA			4001:a1:1014::8b"),
+				test.MX("ipv6.cloud.example.com.			300	IN	MX		1	mxa-ipv6.cloud.example.com."),
+			},
+			[]string{"[4001:a1:1014::8a 4001:a1:1014::8b 4001:a1:1014::89]", "[4001:a1:1014::8b 4001:a1:1014::89 4001:a1:1014::8a]",
+				"[4001:a1:1014::89 4001:a1:1014::8a 4001:a1:1014::8b]"},
+			map[int]int{1: 3, 4: 4},
+			[]dns.RR{},
+		},
 	}
-}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			for i := 0; i < 10; i++ {
+				m := newMid()
+				m.res.Answer = test.answer
+				// state, from the previous loop that arrived in the DNS query
+				m.AddRequestOpt(test.requestOpt...)
+				var clientState = NewStateless().Shuffle(m.req, m.res)
 
-func TestRoundRobinStatelessShuffleAAAA(t *testing.T) {
-	var aaaarotations = []string{"[4001:a1:1014::8a 4001:a1:1014::8b 4001:a1:1014::89]", "[4001:a1:1014::8b 4001:a1:1014::89 4001:a1:1014::8a]",
-		"[4001:a1:1014::89 4001:a1:1014::8a 4001:a1:1014::8b]"}
-	var rr []dns.RR
-	var cname = test.CNAME("ipv6.cloud.example.com.	300	IN	CNAME		beta.cloud.example.com.")
+				// save the new state for the next query
+				test.requestOpt = filterAandAAAA(clientState)
 
-	for i := 0; i < 10; i++ {
-		m := newMid()
-		m.AddResponseAnswer(test.AAAA("ipv6.cloud.example.com.		300	IN	AAAA			4001:a1:1014::89"))
-		m.AddResponseAnswer(cname)
-		m.AddResponseAnswer(test.AAAA("ipv6.cloud.example.com.		300	IN	AAAA			4001:a1:1014::8a"))
-		m.AddResponseAnswer(test.AAAA("ipv6.cloud.example.com.		300	IN	AAAA			4001:a1:1014::8b"))
-		// state, from the previous loop that arrived in the DNS query
-		m.AddRequestOpt(rr...)
-		var clientState = NewStateless().Shuffle(m.req, m.res)
+				if fmt.Sprintf("%v", getIPs(clientState)) != fmt.Sprintf("%v", test.expectedResponse[i%len(test.expectedResponse)]) {
+					t.Errorf("%v: The stateless rotation is not working. Expecting %v but got %v.", i, test.expectedResponse[i%len(test.expectedResponse)], getIPs(clientState))
+				}
 
-		// save the new state for the next query
-		rr = filterAandAAAA(clientState)
+				if len(clientState) != len(m.res.Answer) {
+					t.Errorf("The stateless retrieved different number of records. Expected %v got %v", len(m.res.Answer), len(clientState))
+				}
 
-		if fmt.Sprintf("%v", getIPs(clientState)) != fmt.Sprintf("%v", aaaarotations[i%len(aaaarotations)]) {
-			t.Errorf("%v: The stateless rotation is not working. Expecting %v but got %v.", i, aaaarotations[i%len(aaaarotations)], getIPs(clientState))
-		}
-
-		if len(clientState) != len(m.res.Answer) {
-			t.Errorf("The stateless retrieved different number of records. Expected %v got %v", len(m.res.Answer), len(clientState))
-		}
-
-		if clientState[3].String() != cname.String() {
-			t.Errorf("Expecting %s result but got %s", cname, clientState[3].String())
-		}
+				for originalPosition, newPosition := range test.expectedNonAPositionsMapping {
+					if clientState[newPosition].String() != test.answer[originalPosition].String() {
+						t.Errorf("Expecting %s result but got %s", test.answer[originalPosition], clientState[newPosition])
+					}
+				}
+			}
+		})
 	}
 }
 
