56436e6e464b133b3eeaae23a677238dbe87f38a
kuritka <kuritka@gmail.com>
2022-04-27 10:55:05 +0200

    renaming files

---
+++ plugin/roundrobin/internal/strategy/stateful_impl.go
@@ -0,0 +1,125 @@
+package strategy
+
+import (
+	"fmt"
+	"github.com/coredns/coredns/request"
+	"github.com/miekg/dns"
+	"time"
+)
+
+const (
+	//TODO: garbage collection
+	// defaultTTLSeconds defines when state object is garbage collected
+	defaultTTLSeconds = 600
+	// garbageCollectionPeriodSeconds defines the period when garbage collection is triggered
+	garbageCollectionPeriodSeconds = 1200
+	missingSubnet                  = "missing-subnet"
+	emptySubnet                    = "empty-subnet"
+)
+
+// <clientIP>_<clientSubnet>
+type key string
+
+// one client could hit many domains
+type question string
+
+type state struct {
+	timestamp time.Time
+	ip        []string
+}
+
+type stateful struct {
+	state map[key]map[question]state
+}
+
+func newStateful() *stateful {
+	return &stateful{
+		state: make(map[key]map[question]state),
+	}
+}
+
+func (s *stateful) handle(req *request.Request, res *dns.Msg) (rr []dns.RR, err error) {
+	if req == nil {
+		err = fmt.Errorf("nil request")
+		return
+	}
+	if len(req.Req.Question) == 0 {
+		err = fmt.Errorf("empty request question")
+		return
+	}
+	return s.updateState(req, res)
+}
+
+func (s *stateful) updateState(req *request.Request, res *dns.Msg) (answer []dns.RR, err error) {
+	q := question(req.Req.Question[0].Name)
+	k := s.key(req)
+	responseA, responseIPs, responseNoA := parseAnswerSection(res.Answer)
+	s.refresh(k, q, responseA, responseIPs)
+	for _, ip := range s.state[k][q].ip {
+		answer = append(answer, responseA[ip])
+	}
+	return append(answer, responseNoA...), nil
+}
+
+func (s *stateful) key(req *request.Request) key {
+	subnet := s.readSubnet(req.Req)
+	return key(fmt.Sprintf("%s", subnet))
+}
+
+// readSubnet reads the option EDNS0_SUBNET which is usually filled by resolvers.
+func (s *stateful) readSubnet(req *dns.Msg) string {
+	for _, e := range req.Extra {
+		opt := e.(*dns.OPT)
+		if opt == nil {
+			continue
+		}
+		for _, o := range opt.Option {
+			x := o.(*dns.EDNS0_SUBNET)
+			if x == nil {
+				continue
+			}
+			if o.(*dns.EDNS0_SUBNET).Address.String() == "<nil>" {
+				return emptySubnet
+			}
+			return o.(*dns.EDNS0_SUBNET).Address.String()
+		}
+	}
+	return missingSubnet
+}
+
+func (s *stateful) refresh(k key, q question, responseA map[string]dns.RR, responseIPs []string) {
+	var st state
+	if _, found := s.state[k]; !found {
+		s.state[k] = make(map[question]state)
+	}
+	if _, found := s.state[k][q]; !found {
+		s.state[k][q] = state{
+			ip:        []string{},
+			timestamp: time.Now(),
+		}
+	}
+	st = s.state[k][q]
+	st.updateState(responseA, responseIPs)
+	st.ip = rotate(st.ip)
+	s.state[k][q] = st
+}
+
+func (s *state) updateState(responseA map[string]dns.RR, responseIPs []string) {
+	var newIPs []string
+	currentA := ipsToSet(s.ip)
+
+	// append only such IP which exist in response
+	for _, ip := range s.ip {
+		if _, found := responseA[ip]; found {
+			newIPs = append(newIPs, ip)
+		}
+	}
+
+	// to the end of the IP list append new records which doesn't exist in request but exist in response.
+	for _, ip := range responseIPs {
+		if !currentA[ip] {
+			newIPs = append(newIPs, ip)
+		}
+	}
+	s.ip = newIPs
+}
---
+++ plugin/roundrobin/internal/strategy/stateless_impl.go
@@ -0,0 +1,99 @@
+package strategy
+
+import (
+	"encoding/json"
+	"github.com/miekg/dns"
+	"strings"
+)
+
+type stateless struct {
+	IPs []string `json:"ip"`
+	// IPs converted into map
+	requestIPs map[string]bool
+	// response.Answers[] converted into map which are A or AAAA. IP is a key
+	responseA map[string]dns.RR
+	// contains all response IPs which are A or AAAA
+	responseIPs []string
+	// contains all response records which are not A nor AAAA
+	responseNoA []dns.RR
+}
+
+func newStateless(request *dns.Msg, response *dns.Msg) (s *stateless) {
+	const identifierPrefix = "_rr_state="
+	var empty = func() *stateless {
+		return &stateless{
+			requestIPs:  map[string]bool{},
+			responseA:   map[string]dns.RR{},
+			responseIPs: []string{},
+			responseNoA: []dns.RR{},
+		}
+	}
+	if request == nil || response == nil {
+		return
+	}
+	s = empty()
+	// extracting records from client request
+	for _, e := range request.Extra {
+		opt := e.(*dns.OPT)
+		if opt == nil {
+			continue
+		}
+		for _, o := range opt.Option {
+			e := o.(*dns.EDNS0_LOCAL)
+			if e == nil {
+				continue
+			}
+			data := string(o.(*dns.EDNS0_LOCAL).Data)
+			if strings.HasPrefix(data, identifierPrefix) {
+				str := strings.Replace(data, identifierPrefix, "", -1)
+				_ = json.Unmarshal([]byte(str), &s)
+				if s == nil {
+					s = empty()
+				}
+			}
+		}
+	}
+	s.responseA, s.responseIPs, s.responseNoA = parseAnswerSection(response.Answer)
+	s.requestIPs = ipsToSet(s.IPs)
+
+	return
+}
+
+// updateState compare stateless records with response message records
+// and cuts removed records or append new records to stateless
+// updateState keeps records in the same order as they ar defined in the IPs field.
+func (s *stateless) updateState() *stateless {
+	var newIPs []string
+
+	// append only such IP which exist in response
+	for _, ip := range s.IPs {
+		if _, found := s.responseA[ip]; found {
+			newIPs = append(newIPs, ip)
+		}
+	}
+
+	// to the end of the IP list append new records which doesn't exist in OPT but exist in response.
+	for _, ip := range s.responseIPs {
+		if !s.requestIPs[ip] {
+			newIPs = append(newIPs, ip)
+		}
+	}
+
+	s.IPs = newIPs
+	return s
+}
+
+// rotate performs a cyclic rotation of the IPs records
+func (s *stateless) rotate() *stateless {
+	s.IPs = rotate(s.IPs)
+	return s
+}
+
+// getAnswers recreates Answer slice from original answers
+func (s *stateless) getAnswers() []dns.RR {
+	var shuffled []dns.RR
+	for _, ip := range s.IPs {
+		shuffled = append(shuffled, s.responseA[ip])
+	}
+	return append(shuffled, s.responseNoA...)
+}
